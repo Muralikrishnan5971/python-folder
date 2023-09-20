62ba73ad32437f3c7e751ecb959f6f50ee8f99fe
muralikrishnan <muralikrishnan.pro@gmail.com>
2023-09-20 16:48:27 +0530

    add even number, dummy dict

--- src/dummy_dict.py
+++ src/dummy_dict.py
@@ -29,3 +29,14 @@ print("My name is {} {} and i love"
 expected_toast_msg = (f"Employee '{first} {last}' "
 					   "details has been created successfully")
 print(expected_toast_msg)
+
+exp = {'emp_id': 36849, 
+ 'email_id': 'juliewatson@mailinator.com', 
+ 'first_name': 'Julie', 
+ 'last_name': 'Watson', 
+ 'role': 'Controller', 
+ 'job_title': 'Controller', 
+ 'department_type': 'Service', 
+ 'deapartment': '04 - Mechanical'} 
+
+act = {'emp_id': '36849', 'email_id': 'juliewatson@mailinator.com', 'first_name': 'Julie', 'last_name': 'Watson', 'role': 'Controller', 'job_title': 'Controller', 'department_type': 'Service', 'deapartment': '04 - Mechanical'}
